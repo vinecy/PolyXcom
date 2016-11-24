5718ff80a1a417e0a6d5eb37e2b153f4c4c5ea64
Valentin BISSUEL <Valentin BISSUEL@GE62QF247XFR>
2016-11-24 08:40:25 +0100

    Creation du main et de carte

---
+++ PolyXcom/src/Carte.cpp
@@ -0,0 +1,18 @@
+/*
+ * Carte.cpp
+ *
+ *  Created on: 24 nov. 2016
+ *      Author: Valentin BISSUEL
+ */
+
+#include "Carte.h"
+
+Carte::Carte() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Carte::~Carte() {
+	// TODO Auto-generated destructor stub
+}
+
--- PolyXcom/src/main.cpp
+++ PolyXcom/src/main.cpp
@@ -30,3 +30,8 @@ int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
+
+
+
+
+

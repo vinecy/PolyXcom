2c8681bbcc4fb78faceef8ffb32213e2352a0e4e
Valentin BISSUEL <Valentin BISSUEL@GE62QF247XFR>
2016-11-24 08:42:18 +0100

    ajout de carte.h et de son test unitaire

---
+++ PolyXcom/src/Carte.h
@@ -0,0 +1,17 @@
+/*
+ * Carte.h
+ *
+ *  Created on: 24 nov. 2016
+ *      Author: Valentin BISSUEL
+ */
+
+#ifndef CARTE_H_
+#define CARTE_H_
+
+class Carte {
+public:
+	Carte();
+	virtual ~Carte();
+};
+
+#endif /* CARTE_H_ */
---
+++ PolyXcom/src/Carte_test.cpp
@@ -0,0 +1,9 @@
+/*
+ * Carte_test.cpp
+ *
+ *  Created on: 24 nov. 2016
+ *      Author: Valentin BISSUEL
+ */
+
+#include "Carte.h"
+

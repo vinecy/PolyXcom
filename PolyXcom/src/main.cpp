9072bb3484491dedd91fa8a3590a6b46de8440cf
Valentin BISSUEL <Valentin BISSUEL@GE62QF247XFR>
2016-11-24 08:39:19 +0100

    Creation du main

--- PolyXcom/src/main.cpp
+++ PolyXcom/src/main.cpp
@@ -1,10 +1,27 @@
-//============================================================================
-// Name        : PolyXcom.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, modif de vincent
-//============================================================================
+/**
+ * Polytech Marseille
+ * Case 925 - 163, avenue de Luminy
+ * 13288 Marseille CEDEX 9
+ *
+ * Ce fichier est l'oeuvre d'élèves de Polytech Marseille. Il ne peut être
+ * reproduit, utilisé ou modifié sans l'avis express de ses auteurs.
+ */
+
+/**
+ * @author BARTHOLOMEAU Vincent <vincent.bartholomeau@etu.univ-amu.fr>
+ * @author BISSUEL Valentin <valentin.bissuel@etu.univ-amu.fr>
+ *
+ * @version 0.0.1 / 24/11/2016
+ *
+ * @todo -
+ *
+ * @bug -
+ */
+
+/**
+ * @file main.c
+ * @brief Le main permet de lancer le jeu.
+ */
 
 #include <iostream>
 using namespace std;

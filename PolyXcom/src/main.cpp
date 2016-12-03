c7ce4917d04e169f4c359ad79be644c5e606f54a
Valentin BISSUEL <Valentin BISSUEL@GE62QF247XFR>
2016-12-03 09:57:46 +0100

    Main contenant les tests de path à merger

--- PolyXcom/src/main.cpp
+++ PolyXcom/src/main.cpp
@@ -28,6 +28,7 @@
 #include "Arme.h"
 #include "Graphe.h"
 #include "Ennemi.h"
+#include "Obstacle.h"
 using namespace std;
 
 #define DEPLACER 1
@@ -51,6 +52,30 @@ int choisir ( void ){
 
 
 int main() {
+
+	Carte luminy(6,6);
+	Hero val(1,1,2,10,10,Arme(),"Valentin");
+	Ennemi pros(4,2,3,10,10,Arme());
+	Obstacle mur1(3,0,1);
+	Obstacle mur2(3,1,1);
+	Obstacle mur3(3,2,1);
+	Obstacle mur4(3,3,1);
+	Obstacle mur5(3,4,1);
+	Obstacle mur6(2,3,1);
+
+	luminy.addItem(val);
+	luminy.addItem(pros);
+	luminy.addItem(mur1);
+	luminy.addItem(mur2);
+	luminy.addItem(mur3);
+	luminy.addItem(mur4);
+	luminy.addItem(mur5);
+	luminy.addItem(mur6);
+
+	luminy.display();
+
+	luminy.pathfinding(1, 1, 4, 3);
+
 	/*int choix;
 
 	//system("cls");

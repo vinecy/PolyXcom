75bbf54db122fe503223423f9c6ebc72b021aa74
vinecy <vinecy@hotmail.fr>
2017-02-03 13:06:44 +0100

    commit "mais que quoi?"

--- PolyXcom/src/Partie.cpp
+++ PolyXcom/src/Partie.cpp
@@ -351,7 +351,7 @@ void Partie::HandleEvents(IHMmanager* game){
 										_ite_p++;
 									}
 									cout <<"portail trouvé"<<endl;
-									//switchMap((*_ite_p)); //TODO Switch
+									switchMap((*_ite_p)); //TODO Switch
 								}
 							}
 							/*
@@ -418,16 +418,14 @@ void Partie::Update(IHMmanager* game){
 				// mode exploration
 				cout << " Mode exploration " << endl;
 				// TODO à remettre au propre
-				_ite_l = _team_hero.begin();			// itérateur sur le héros principal car il est le seul à se déplacer
-				cout << "tes cense faire un truc \n";
-				_ite_l++;								// on retire les compagnons de la carte
-				cout << "tes cense faire un truc 2\n";
-				while( _ite_l != _team_hero.end()){
-					_mapCurrent.removeItem(*(*_ite_l));
-					_ite_l++;
+				_ite_h = _tank_hero.begin();
+				_ite_h++;
+				while(_ite_h != _tank_hero.end())
+				{
+					_mapCurrent.removeItem((*_ite_h));
+					_ite_h++;
 				}
-				_ite_h = _tank_hero.end();
-				_ite_l = _team_hero.begin();
+				_ite_h = _tank_hero.begin();
 			} else {
 				// mode combat
 
